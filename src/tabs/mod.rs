4942bdd70fffa7985921c3930d10f675d641266b
Nolan Peterson <ndpete2007@gmail.com>
2026-07-25 23:18:40 -0500

    feat: info tab

--- src/tabs/mod.rs
+++ src/tabs/mod.rs
@@ -1,8 +1,10 @@
 mod configuration;
+mod device_info;
 mod home;
 mod logs;
 
 pub use configuration::ConfigurationTab;
+pub use device_info::DeviceInfoTab;
 pub use home::HomeTab;
 pub use logs::LogsTab;
 
@@ -15,16 +17,18 @@ pub enum Tab {
     Home,
     Logs,
     Configuration,
+    Info,
 }
 
 impl Tab {
-    pub const ALL: [Tab; 3] = [Tab::Home, Tab::Logs, Tab::Configuration];
+    pub const ALL: [Tab; 4] = [Tab::Home, Tab::Logs, Tab::Configuration, Tab::Info];
 
     pub fn title(self) -> &'static str {
         match self {
             Tab::Home => "Home",
             Tab::Logs => "Logs",
             Tab::Configuration => "Configuration",
+            Tab::Info => "Info",
         }
     }
 }
